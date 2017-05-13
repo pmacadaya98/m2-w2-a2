8868c4ffdcbe7cbcd77034c8e9339d231d06d1f8
pmacadaya98 <pmacadaya@gmail.com>
2017-05-13 15:38:14 +0800

    Add files via upload

---
+++ js/script1.js
@@ -0,0 +1,4 @@
+document.querySelector("#appdemo2").innerHTML = "<button class='btn btn-primary' id='showname'> PRESS HERE! </button>";
+$('#showname').click(function(){
+	alert("Peps Macadaya");
+});
